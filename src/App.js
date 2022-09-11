88ad6cea70e490aa9a2944e10119d96ac182d8ff
Mphan87 <102190333+Mphan87@users.noreply.github.com>
2022-09-11 16:14:33 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Eightball from './eightball.js'
+
+function App() {
+  return (
+    <div className="App">
+
+   <Eightball/>
+
+     
+    </div>
+  );
+}
+
+export default App;
